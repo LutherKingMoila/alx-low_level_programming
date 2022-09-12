b9d2f8ae16c88a7bfd19c1aebb6991f2f3224f7d
LutherKingMoila <luthermoila7@gmail.com>
2022-09-12 14:21:35 -0700

    i understand now

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char c;
+long int li;
+long long int lli;
+float f;
+printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
+printf("Size of an long int: %lu.\n", (unsigned long)sizeof(lli));
+printf("Size of an float: %lu.\n", (unsigned long)sizeof(f));
+return (0);
+}
