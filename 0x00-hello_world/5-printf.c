046895c0f957d6062263a3934a7faf0d60625443
LutherKingMoila <luthermoila7@gmail.com>
2022-09-12 07:41:58 -0700

    still very hard

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-printf("ith proper grammar, but the outcome is a piece of art \n"); return (0)
+#include<stdio.h>
+int main(void)
+{
+printf("ith proper grammar, but the outcome is a piece of art \n")
+}
